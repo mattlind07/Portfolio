1636a57542558b4251a42a8af154e929038443a8
Matthew Lindsey <mal00026@mix.wvu.edu>
2025-11-10 21:03:32 -0500

    Mobile CSS Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,38 @@
 import logo from '../assets/MLlogo.png';
 import {Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         
         <nav className="navbar">
         <div className='branding'>
-        <Link to="/">   
+        <Link to="/" onClick={closeMenu}>   
           <img className="logo" src={logo} alt="Logo" />
         </Link>
           <h1 className="Name">Matthew Lindsey</h1>
         </div>
-          <ul>
-            <Link to="/about" className="navbutton">About Me</Link>
-            <Link to="/projects" className="navbutton">Projects</Link>
-            <Link to="/resume" className="navbutton">Resume</Link>
-            <Link to="/contact" className="navbutton">Contact</Link>
+        <button className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu} aria-label="Toggle menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+          <ul className={`nav-menu ${isMenuOpen ? 'nav-menu-open' : ''}`}>
+            <li><Link to="/about" className="navbutton" onClick={closeMenu}>About Me</Link></li>
+            <li><Link to="/projects" className="navbutton" onClick={closeMenu}>Projects</Link></li>
+            <li><Link to="/resume" className="navbutton" onClick={closeMenu}>Resume</Link></li>
+            <li><Link to="/contact" className="navbutton" onClick={closeMenu}>Contact</Link></li>
           </ul>
         </nav>
 
